3c9fbf3c117b4997999da771bccc83ae61b86172
haoxiaozi-libing <1445935345@qq.com>
2021-03-19 23:51:43 +0800

    好开心啊  生效了，Eureka注解没有标注

--- cloud-provider-hystrix-payment8001/src/main/java/com/libing/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/libing/springcloud/PaymentHystrixMain8001.java
@@ -2,6 +2,8 @@ package com.libing.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 
@@ -10,7 +12,8 @@ import org.springframework.context.ConfigurableApplicationContext;
  * @create 2021-03-17 23:31
  */
 @SpringBootApplication
-@EnableFeignClients
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(PaymentHystrixMain8001.class, args);
