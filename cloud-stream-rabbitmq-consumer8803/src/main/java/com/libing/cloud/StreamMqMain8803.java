0ccbca7a10a6e5de2724968236c855d99a4be7a3
lvlibing <1445935345@qq.com>
2021-05-22 10:09:13 +0800

    下一讲分组

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/libing/cloud/StreamMqMain8803.java
@@ -0,0 +1,29 @@
+package com.libing.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author lvlibing
+ * @create 2021-05-20 23:46
+ */
+
+// 消息驱动之生产者
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamMqMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMqMain8803.class,args);
+    }
+/*springCloud是一个构建消息驱动微服务的框架
+* 应用程序通过inputs或者outputs来有springCloud的Stream中的binder对象交互
+*input生产者
+* output：消费者
+* Stream中的消息通信方式遵循发布订阅模式--》topic主题进行广播，在rabbitMq中就是交换机，kafka中就是Topic
+* 通过定义binder作为中间层，实现了应用程序与消息中间件细节之间的隔离
+*
+*
+*
+* */
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/libing/cloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,25 @@
+package com.libing.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lvlibing
+ * @create 2021-05-22 9:42
+ */
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageListenerController {
+@Value("${server.port}")
+private String serverPort;
+
+@StreamListener(Sink.INPUT)
+public void input(Message<String> message){
+    System.out.println("消费者2号,收到的消息"+message.getPayload()+"---端口--->"+serverPort);
+}
+
+}
